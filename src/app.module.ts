f0c55574fbedfe50426c05a1ba01149d9f94f032
Shubham Patidar <shubhampatidar@theninjastudio.com>
2025-12-25 10:46:50 +0530

    updated response on base route

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { PublicModule } from './modules/public/public.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { AttendanceModule } from './modules/attendance/attendance.module';
 import { TeacherModule } from './modules/teacher/teacher.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -16,5 +18,7 @@ import { TeacherModule } from './modules/teacher/teacher.module';
     AttendanceModule,
     TeacherModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
